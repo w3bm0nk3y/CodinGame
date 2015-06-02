898292f94ccd71acca6014b897e9d7a6f9e90d3c
w3bm0nk3y <w3bm0nk3y@users.noreply.github.com>
2015-06-02 11:35:31 -0700

    https://app.asana.com/0/36342360371702/36342360371706 Copied initial code for Temperatures

---
+++ Temperatures/Temperatures/Temperatures.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+/**
+* Auto-generated code below aims at helping you parse
+* the standard input according to the problem statement.
+**/
+int main()
+{
+    int N; // the number of temperatures to analyse
+    cin >> N; cin.ignore();
+    string TEMPS; // the N temperatures expressed as integers ranging from -273 to 5526
+    getline(cin, TEMPS);
+
+    // Write an action using cout. DON'T FORGET THE "<< endl"
+    // To debug: cerr << "Debug messages..." << endl;
+
+    cout << "result" << endl;
+}
